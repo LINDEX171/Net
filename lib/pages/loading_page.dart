4ee42d1b76e2248e28ffdaffa9d348adddfb10d2
LINDEX171 <lindex171@gmail.com>
2024-12-05 16:37:41 +0000

    animation and reirection done

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:netflix/pages/home_page.dart';
+import 'package:netflix/pages/myhome_page.dart';
 
 class LoadingPage extends StatefulWidget {
   const LoadingPage({super.key});
@@ -9,11 +13,30 @@ class LoadingPage extends StatefulWidget {
 }
 
 class _LoadingPageState extends State<LoadingPage> {
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadAnimation();
+  }
+
+  Future<Timer> loadAnimation() async {
+    return  Timer(Duration(seconds: 4), onLoaded);
+  }
+
+ onLoaded(){
+   Navigator.of(context).pushReplacement(
+     MaterialPageRoute(builder: (context) => MyhomePage(),)
+   );
+ }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(child: Lottie.asset("assets/lotties/netflix.json"),)
+      body: Center(child: Lottie.asset("assets/lotties/netflix.json",repeat: false),)
     );
   }
 }
