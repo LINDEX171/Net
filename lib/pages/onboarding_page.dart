013f9af7b925c404acb7d08ec161320a1b782739
LINDEX171 <lindex171@gmail.com>
2024-12-10 16:18:18 +0000

    3 rounded points & button

--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -11,15 +11,46 @@ class _OnboardingPageState extends State<OnboardingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Stack(
         children: [
           PageView(
-            children: [
-              Center(child: Text("h")),
-              Center(child: Text("hi")),
-              Center(child: Text("ho"))
-            ],
-          )
+            children: [Text("h"), Text("hi"), Text("ho")],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(
+                    3,
+                    (index) => AnimatedContainer(
+                      duration: Duration(milliseconds: 500),
+                      margin: EdgeInsets.only(right: 5),
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      height: 10,
+                      width: 10,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Get started"),
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusDirectional.horizontal()),
+                        backgroundColor: Colors.red,foregroundColor: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
