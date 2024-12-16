1c23f574e26f023c322d5d3dc63932167d1a8ea7
LINDEX171 <lindex171@gmail.com>
2024-12-16 13:25:38 +0000

    validation

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,9 +8,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
- 
- 
-   bool _obscureText = true ;
+  final _formKey = GlobalKey<FormState>();
+  final _emailTextField =  TextEditingController();
+  final _passwordTextField = TextEditingController();
+   bool _obscureText = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,10 +41,14 @@ class _LoginPageState extends State<LoginPage> {
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: SingleChildScrollView(
           child: Form(
+            key: _formKey,
               child: Column(
             children: [
-              SizedBox(height: 60,),
+              SizedBox(
+                height: 60,
+              ),
               TextFormField(
+                controller: _emailTextField,
                 style: TextStyle(
                   color: Colors.white,
                 ),
@@ -56,28 +61,69 @@ class _LoginPageState extends State<LoginPage> {
                       borderRadius: BorderRadius.circular(5),
                       borderSide: BorderSide.none),
                 ),
+                validator: (value) {
+                  if( value == null || value==value.isEmpty){
+                    return "please enter Email";
+                  }else if(!value.contains("@")){
+                    return "please enter a valid email adress";
+                  }else if(!value.contains(".")){
+                    return "please enter a valid email adress";
+                  }
+                  return null ;
+                },
               ),
               SizedBox(height: 20),
               TextFormField(
+                controller: _passwordTextField,
                 obscureText: _obscureText,
                 style: TextStyle(
                   color: Colors.white,
                 ),
                 decoration: InputDecoration(
-                  labelText: "Password",
-                  labelStyle: TextStyle(color: Colors.white),
-                  filled: true,
-                  fillColor: Colors.grey[800],
-                  border: UnderlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: BorderSide.none),
-                suffixIcon: IconButton(onPressed: () {
-                     setState(() {
-                       _obscureText = !_obscureText;
-                     });
-                }, icon: _obscureText ? Icon(Icons.visibility,color: Colors.white,):Icon(Icons.visibility_off,color: Colors.white,) )
-                ),
-                
+                    labelText: "Password",
+                    labelStyle: TextStyle(color: Colors.white),
+                    filled: true,
+                    fillColor: Colors.grey[800],
+                    border: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: BorderSide.none),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _obscureText = !_obscureText;
+                          });
+                        },
+                        icon: _obscureText
+                            ? Icon(
+                                Icons.visibility,
+                                color: Colors.white,
+                              )
+                            : Icon(
+                                Icons.visibility_off,
+                                color: Colors.white,
+                              ))),
+                validator: (value) {
+                  if( value == null || value.isEmpty){
+                    return "please enter password";
+                  }
+                  return null ;
+                },
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.red,foregroundColor: Colors.white,shape: RoundedRectangleBorder()),
+                    onPressed: () {
+                      if(_formKey.currentState!.validate()){
+                      //do something
+                        print(_emailTextField.text);
+                        print(_passwordTextField.text);
+                      }
+
+                    }, child: Text("Sign In",style: TextStyle(fontSize: 20),)),
               )
             ],
           )),
