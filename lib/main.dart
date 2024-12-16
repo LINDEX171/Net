ba2ad909ad0c140c2f673f086263d7a0114dba18
LINDEX171 <lindex171@gmail.com>
2024-12-16 15:53:51 +0000

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix/firebase_options.dart';
 import 'package:netflix/pages/loading_page.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
